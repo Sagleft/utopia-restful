0831700cbb3787b70598632e5a93d66da8eaa7e4
UtopistMan <digital@mfc.click>
2021-09-20 21:47:23 +0400

    add placeholder page

--- public_html/index.php
+++ public_html/index.php
@@ -1,3 +1,17 @@
-<?php
-	echo 'placeholder';
-	
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Title</title>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/uikit@3.7.3/dist/css/uikit.min.css" />
+		    <link rel="stylesheet" href="/css/main.css" />
+    </head>
+    <body>
+      <div class="uk-container uk-text-center">
+		    <p class="uk-text-lead">PLACEHOLDER</p>
+      </div>
+      <script src="https://cdn.jsdelivr.net/npm/uikit@3.7.3/dist/js/uikit.min.js"></script>
+      <script src="https://cdn.jsdelivr.net/npm/uikit@3.7.3/dist/js/uikit-icons.min.js"></script>
+    </body>
+</html>
